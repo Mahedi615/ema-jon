37b1509388217d6b1bdf3abdc8a79603f454709d
Mahedi <devmahedi615@gmail.com>
2023-05-06 23:26:35 +0600

    initial commit

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <h6>This is Shop</h6>
+        </div>
+    );
+};
+
+export default Shop;
